4fb6ca9857004f1d2d6fbac31e8b2ff2a9f6cc40
fietsserviceid <support@fietsserviceid.nl>
2026-01-28 19:33:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker v9.3 – root scope met 'zoals-het-was' gedrag voor codes/data
-const CACHE_NAME = 'fiets-inruil-cache-v9.3';
+// Service Worker v9.4 – root scope met 'zoals-het-was' gedrag voor codes/data
+const CACHE_NAME = 'fiets-inruil-cache-v9.4';
 const ASSETS = [
   '/', '/index.html', '/styles.css', '/app.js',
   '/manifest.webmanifest', '/favicon.ico',
